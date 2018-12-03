5f562e51068ef35433b799019ecb0145c9149169
Shail Patel <shail@spm-automation.com>
2018-12-03 14:58:42 -0500

    contact me upates

--- aboutme/html/welcome.php
+++ aboutme/html/welcome.php
@@ -1,4 +1,10 @@
- <?php 
-$text = $_Post["first"];
-echo "text";
- ?>
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "shail@spm-automation.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
